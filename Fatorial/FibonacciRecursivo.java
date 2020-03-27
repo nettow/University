9beeb73e3dc811727bf82df76dcbc7b840abf0ce
nettow <jlsnsilveira@sga.pucminas.br>
2020-03-27 20:42:16 -0300

    Fibonacci Recursivo

---
+++ Fatorial/FibonacciRecursivo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class FibonacciRecursivo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o n-esimo termo de fibonnaci: ");
+        int fib = sc.nextInt();
+        System.out.println("O " + fib + " termo de fibonaccié igual a: " + fibonnaciIterativo (fib));
+        System.out.println(fibonnaciRecursivo(fib));  
+    }
+
+    private static int fibonnaciRecursivo(int n){
+        if ((n == 0) || (n == 1)){
+            return 1;
+        }
+        else
+            return fibonnaciRecursivo(n-1) + fibonnaciRecursivo(n-2);
+    }
+}
